667606e652cab8a54c61a028862e051b5b92e856
khushisingh2008-coder <khushisingh1970@gmail.com>
2025-08-22 11:14:10 +0530

    Game

---
+++ app.js
@@ -0,0 +1,78 @@
+let boxes= document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let newBtn = document.querySelector(".winner");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
+let turnO = true;
+let count = 0;
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+const resetGame = () => {
+    trueO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+} 
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+                if(turnO) {
+            box.innerText = "O";
+            turnO = false;
+        }
+        else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+        count++;
+        let iswinner = checkWinner();
+        if (count === 9 && !iswinner) {
+            gameDraw();
+        }
+    });
+});
+const gameDraw = () => {
+    msg.innerText = `Game was Draw.`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let position1val = boxes[pattern[0]].innerText;
+        let position2val = boxes[pattern[1]].innerText;
+        let position3val = boxes[pattern[2]].innerText;
+    if (position1val != "" && position2val != "" && position3val != "") {
+         if (position1val === position2val && position2val === position3val){
+             
+            showWinner(position1val);
+            }
+         }
+    }
+};
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
